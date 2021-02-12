68536316d233228c67a9355ae0e979667225f2b7
Manan Paruthi <manan.paruthi@gmail.com>
2021-02-12 19:35:36 +0530

    Update Cheat Sheet.py

--- Cheat Sheet.py
+++ Cheat Sheet.py
@@ -107,6 +107,7 @@ def bubbleSort(a):
         else:
             break
     return {noOfSwaps, a}
+bubbleSort([5,4,1,2,3])
 
 #bisect - for maintaining a list in sorted order without having to sort the list after each insertion (uses Array bisection algorithm)
 import bisect
@@ -116,3 +117,28 @@ print(bisect.bisect_right(arr, 1)) #3 returns next index of right most ele
 array = [0,2,3] #array should be sorted
 bisect.insort(array, 1) #inserts value at the perfect position in sorted array  
 print(array) #[0, 1, 2, 3]
+
+# Merge Sort
+def merge(a1, a2):
+    swaps, i, j, result, m, n = 0, 0, 0, [], len(a1), len(a2)
+    while i < m and j < n:
+        if a1[i] <= a2[j]:
+            result.append(a1[i])
+            i += 1
+        else:
+            result.append(a2[j])
+            j += 1
+            swaps += m - i
+    result += a1[i:]
+    result += a2[j:]    
+    return swaps, result    
+def msort(arr):
+    n = len(arr)
+    mid = n // 2
+    if n > 1:
+        left_swaps, left_result = msort(arr[:mid])
+        right_swaps, right_result = msort(arr[mid:])
+        m_swaps, result = merge(left_result, right_result)
+        return m_swaps + left_swaps + right_swaps, result
+    return 0, arr
+msort([5,4,1,2,3])
