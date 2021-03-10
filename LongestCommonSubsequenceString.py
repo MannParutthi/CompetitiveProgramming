7204e686edd19066c9a139daf0ca6560fe96c558
Manan Paruthi <manan.paruthi@gmail.com>
2021-03-10 10:42:52 +0530

    Create LongestCommonSubsequenceString.py

---
+++ LongestCommonSubsequenceString.py
@@ -0,0 +1,30 @@
+def LCS(X, Y):
+    m = len(X) 
+    n = len(Y)
+    L = [[None]*(n + 1) for i in range(m + 1)]
+    
+    for i in range(m + 1): 
+        for j in range(n + 1): 
+            if i == 0 or j == 0 : 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1]+1
+            else:
+                L[i][j] = max(L[i-1][j], L[i][j-1])
+    print('Length of LCS:',L[m][n])
+    
+    i = m
+    j = n
+    lcsStr = ''
+    while i > 0 and j > 0:
+      if X[i-1] == Y[j-1]:
+        lcsStr += X[i-1]
+        i-=1
+        j-=1
+      elif L[i-1][j] > L[i][j-1]:
+        i-=1
+      else:
+        j-=1
+    print('LCS String:', lcsStr[::-1])
+        
+LCS('SHINCHAN', 'NOHARAAA')
