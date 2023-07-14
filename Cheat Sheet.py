9a119364c36017223996c547987a165b08a23c98
Manan Paruthi <manan.paruthi@gmail.com>
2023-07-14 11:19:08 -0400

    Update Cheat Sheet.py

--- Cheat Sheet.py
+++ Cheat Sheet.py
@@ -176,4 +176,8 @@ import collections
 q = collections.deque()
 q.append(1)
 q.append(2) 
-q.popleft() # 1 => returns first ele FIFO
+q.popleft() # 1 => pops first ele FIFO
+q.pop() # 2 => pops last ele X 
+
+dict = collections.defaultdict(list) # default dict ; all the values with ve initialized as [] empty list by default
+dict['A'].append['B'] # => A: [B] => no need to initialize like if 'A' not in dict then dict['A'] = [] and then append
