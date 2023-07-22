03f95624dd28832b3e515c095e1bf453c7bd2128
Manan Paruthi <manan.paruthi@gmail.com>
2023-07-22 18:11:17 -0400

    Update Cheat Sheet.py

--- Cheat Sheet.py
+++ Cheat Sheet.py
@@ -211,3 +211,6 @@ matrix = [
 ]
 transposedMatrix = list(zip(*matrix)) # gives list of tuples
 print("transposedMatrix => ", transposedMatrix) # [(1, 4, 7), (2, 5, 8), (3, 6, 9)]
+
+arr = [1,2,3,4,5,6,7,8]
+print("Random Choice with equal probability", random.choice(arr)) # O(logn) / O(n)
